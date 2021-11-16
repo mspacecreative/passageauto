127ac99394707ecab6dc6d0525a30e6cbe058a52
Matt <matt@mspacecreative.com>
2021-11-16 07:57:51 -0400

    c

--- functions/shortcodes.php
+++ functions/shortcodes.php
@@ -6,3 +6,11 @@ function featured_product() {
 	return ob_get_clean();
 }
 add_shortcode( 'featured_product', 'featured_product' );
+
+// GOOGLE MAP
+function google_map() {
+	ob_start();
+		get_template_part('templates/google-map');
+	return ob_get_clean();
+}
+add_shortcode( 'google_map', 'google_map' );
---
+++ templates/google-map.php
@@ -0,0 +1,5 @@
+<?php 
+$map = get_field('location', 'options');
+if ($map) {
+	echo $map;
+}
