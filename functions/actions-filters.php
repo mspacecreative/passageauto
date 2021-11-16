e93c0e13ccbfabf5063ea107f1ab71dbcf0473e1
Matt <matt@mspacecreative.com>
2021-11-16 09:28:59 -0400

    c

--- functions/actions-filters.php
+++ functions/actions-filters.php
@@ -1,5 +1,6 @@
 <?php 
 add_action('et_header_top', 'custom_navigation');
 function custom_navigation() {
-	return '<a class="et_pb_button header_button" href="#">New Button</a>';
+	$nav = '<a class="et_pb_button header_button" href="#">New Button</a>';
+	return $nav;
 }
