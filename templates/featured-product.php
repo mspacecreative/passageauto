e5b852d7dd33ccfbd388424279c4fa9ac290ea5d
Matt <matt@mspacecreative.com>
2021-11-19 17:14:46 -0400

    c

--- templates/featured-product.php
+++ templates/featured-product.php
@@ -1,13 +1,15 @@
 <div class="featured-product-container top-bottom-padding">
-	<div class="max-width-1080 top-bottom-padding display-flex gutter-space-1">
-		<div class="col display-flex gutter-space-1">
-			<div class="inner display-flex align-items-center">
-				<?php promo_item('featured-product'); ?>
+	<div class="max-width-1080 top-bottom-padding">
+		<div class="display-flex gutter-space-1">
+			<div class="col display-flex gutter-space-1">
+				<div class="inner display-flex align-items-center">
+					<?php promo_item('featured-product'); ?>
+				</div>
 			</div>
-		</div>
-		<div class="col display-flex gutter-space-1">
-			<div class="inner display-flex align-items-center">
-				<?php promo_item('flyer'); ?>
+			<div class="col display-flex gutter-space-1">
+				<div class="inner display-flex align-items-center">
+					<?php promo_item('flyer'); ?>
+				</div>
 			</div>
 		</div>
 	</div>
