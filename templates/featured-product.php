7637cd8b034f0832a9671506717c7435d08bc1fb
Matt <matt@mspacecreative.com>
2021-11-19 17:18:50 -0400

    c

--- templates/featured-product.php
+++ templates/featured-product.php
@@ -1,12 +1,12 @@
 <div class="featured-product-container top-bottom-padding">
 	<div class="max-width-1080 top-bottom-padding">
-		<div class="display-flex gutter-space-1">
-			<div class="col display-flex gutter-space-1">
+		<div class="display-flex gutter-space-2">
+			<div class="col display-flex">
 				<div class="inner display-flex align-items-center">
 					<?php promo_item('featured-product'); ?>
 				</div>
 			</div>
-			<div class="col display-flex gutter-space-1">
+			<div class="col display-flex">
 				<div class="inner display-flex align-items-center">
 					<?php promo_item('flyer'); ?>
 				</div>
