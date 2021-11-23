b0df978e1359ff3361136bced35ac8ac86870b9b
Matt <matt@mspacecreative.com>
2021-11-23 10:22:10 -0400

    c

--- templates/featured-product.php
+++ templates/featured-product.php
@@ -1,6 +1,6 @@
 <div class="featured-product-container top-bottom-padding">
 	<div class="max-width-1440 top-bottom-padding">
-		<div class="display-flex gutter-space-2">
+		<div class="display-flex gutter-space-2 align-middle">
 			<div class="col display-flex">
 				<div class="inner display-flex">
 					<?php promo_item('featured-product'); ?>
