6bb7df3a6b185500d533af5db8c2f521ec6bef4c
Matt Cyr <matt@mspacecreative.com>
2022-09-26 15:51:59 -0300

    Fixed up canned messaging

--- templates/featured-product.php
+++ templates/featured-product.php
@@ -2,9 +2,12 @@
 	<div class="max-width-1440 top-bottom-padding">
 		<div class="display-flex gutter-space-2 align-middle">
 			<?php 
-			promo_item('featured-product');
-			promo_item('flyer');
-			?>
+			if (function_exists(promo_item('featured-product') || promo_item('flyer'))):
+				promo_item('featured-product');
+				promo_item('flyer');
+			else : ?>
+				<h3 style="text-align: center;"><?php echo esc_html_e('Sorry, there are currently no promotions or featured products.'); ?><br /><?php echo esc_html_e('Please check back soon!'); ?></h3>
+			<?php endif; ?>
 		</div>
 	</div>
 </div>
