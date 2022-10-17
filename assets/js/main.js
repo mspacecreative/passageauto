4ddf01113e813ec8fe7e7747b3705f9479e8ee82
Matt <matt@mspacecreative.com>
2022-10-17 11:32:59 -0300

    Adjusted JS file

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,38 +1,37 @@
-(function($) {
-	var 
-	menuOverlay = $('.menuOverlay'),
-	clientOverlay = $('.clientsContainer'),
-	hamburgerIcon = $('.hamburger'),
-	header = $('header');
-	function toggleMenu() {
-		$('body').toggleClass('menuOpen');
+(function ($) {
+  var menuOverlay = $(".menuOverlay"),
+    clientOverlay = $(".clientsContainer"),
+    hamburgerIcon = $(".hamburger"),
+    header = $("header");
+  function toggleMenu() {
+    $("body").toggleClass("menuOpen");
 
-		hamburgerIcon.toggleClass('is-active');
+    hamburgerIcon.toggleClass("is-active");
 
-		header.toggleClass('is-active');
+    header.toggleClass("is-active");
 
-		menuOverlay.fadeToggle('fast');
+    menuOverlay.fadeToggle("fast");
 
-		/*
+    /*
 		if ( subMenuLink.href.indexOf("#") != -1 ) {
 			hamburgerIcon.removeClass('is-active');
 			$('body').removeClass('menuOpen');
 		}
 		*/
-	}
+  }
 
-	// MOBILE MENU
-	$('.hamburger').click(function(e) {
-		e.preventDefault();
-		toggleMenu();
-	});
-	
-	$('.menuInner .menu-item a').click(function(e) {
-		e.stopPropagation();
-		toggleMenu();
-	});
-	
-	/*
+  // MOBILE MENU
+  $(".hamburger").click(function (e) {
+    e.preventDefault();
+    toggleMenu();
+  });
+
+  $(".menuInner .menu-item a").click(function (e) {
+    e.stopPropagation();
+    toggleMenu();
+  });
+
+  /*
 	// REVEAL FLYER CONTAINER ON CLICK
 	var pdfContainer = $('.flyer-container'),
 		viewPDFButton = $('.view-pdf-container');
@@ -42,92 +41,98 @@
 	});
 	*/
 
-	// CLICK TO OPEN BIOS
-	var 
-	bioContainer = $('.bio-container'),
-	leahButton = $('.leah'),
-	johnnaButton = $('.johnna'),
-	leahBio = $('#bio-390'),
-	johnnaBio = $('#bio-391');
-	topHeaderHeight = $('#top-header').height();
-	
-	leahButton.on('click', function(e) {
-		e.preventDefault();
-		bioContainer.fadeIn();
-		bioContainer.find(leahBio).addClass('reveal');
-	});
+  // CLICK TO OPEN BIOS
+  var bioContainer = $(".bio-container"),
+    leahButton = $(".leah"),
+    johnnaButton = $(".johnna"),
+    leahBio = $("#bio-390"),
+    johnnaBio = $("#bio-391");
+  topHeaderHeight = $("#top-header").height();
 
-	johnnaButton.on('click', function(e) {
-		e.preventDefault();
-		bioContainer.fadeIn();
-		bioContainer.find(johnnaBio).addClass('reveal');
-	});
+  leahButton.on("click", function (e) {
+    e.preventDefault();
+    bioContainer.fadeIn();
+    bioContainer.find(leahBio).addClass("reveal");
+  });
 
-	// CLOSE BIOS VIA ESC BUTTON
-	$(document).on('keydown', function(e) {
-		if ( e.keyCode == 27 || e.keyCode == 'ESC' ) {
-			bioContainer.fadeOut();
-			bioContainer.find('.bio-container-table').removeClass('reveal');
-		}
-	});
+  johnnaButton.on("click", function (e) {
+    e.preventDefault();
+    bioContainer.fadeIn();
+    bioContainer.find(johnnaBio).addClass("reveal");
+  });
 
-	// CLOSE MODAL VIA OUTER OVERLAY
-	bioContainer.on('click', function() {
-		$(this).fadeOut();
-		$(this).find('.bio-container-table').removeClass('reveal');
-	});
+  // CLOSE BIOS VIA ESC BUTTON
+  $(document).on("keydown", function (e) {
+    if (e.keyCode == 27 || e.keyCode == "ESC") {
+      bioContainer.fadeOut();
+      bioContainer.find(".bio-container-table").removeClass("reveal");
+    }
+  });
 
-	$('.bio-container-inner').on('click', function(e) {
-		e.stopPropagation();
-	});
+  // CLOSE MODAL VIA OUTER OVERLAY
+  bioContainer.on("click", function () {
+    $(this).fadeOut();
+    $(this).find(".bio-container-table").removeClass("reveal");
+  });
 
-	$('.close-bio-container').on('click', function() {
-		bioContainer.fadeOut();
-		bioContainer.find('.bio-container-table').removeClass('reveal');
-	});
-	
-	// CLOSE MENU ON LINK CLICK
-	var menItem = $('.et_mobile_menu li'),
-		mobileMenu = $('.et_mobile_menu'),
-		header = $('#main-header'),
-		splashContainer = $('.splash-container'),
-		splash = $('.splash'),
-		viewport = $(window);
-		
-	menItem.click(function() {
-		mobileMenu.slideUp();
-	});
-	
-	function splashHeight() {
-		splash.height(viewport.height() - header.height());
-	}
-	
-	function splashContainerPadding() {
-		splashContainer.css('padding-top', header.height() + topHeaderHeight);
-	}
-	
-	// HIDE/SHOW HEADER ON SCROLL
-	var lastScrollTop = 0;
-	var delta = 5;
-	$(window).on('scroll', function() {
-		var st = $(this).scrollTop();
-		if (st < lastScrollTop) {
-			$('header, body').addClass('up').removeClass('down');
-		} else if (st > lastScrollTop && st > delta) {
-			$('header, body').addClass('down').removeClass('up');
-		}
-		
-		lastScrollTop = st;
-	});
-	
-	$(function ($) {
-		//splashHeight();
-		splashContainerPadding();
-	});
-	
-	viewport.resize(function() {
-		//splashHeight();
-		splashContainerPadding();
-	});
-	
+  $(".bio-container-inner").on("click", function (e) {
+    e.stopPropagation();
+  });
+
+  $(".close-bio-container").on("click", function () {
+    bioContainer.fadeOut();
+    bioContainer.find(".bio-container-table").removeClass("reveal");
+  });
+
+  // CLOSE MENU ON LINK CLICK
+  var menItem = $(".et_mobile_menu li"),
+    mobileMenu = $(".et_mobile_menu"),
+    header = $("#main-header"),
+    splashContainer = $(".splash-container"),
+    splash = $(".splash"),
+    viewport = $(window);
+
+  menItem.click(function () {
+    mobileMenu.slideUp();
+  });
+
+  function splashHeight() {
+    splash.height(viewport.height() - header.height());
+  }
+
+  function splashContainerPadding() {
+    splashContainer.css("padding-top", header.height() + topHeaderHeight);
+  }
+
+  // HIDE/SHOW HEADER ON SCROLL
+  var lastScrollTop = 0;
+  var delta = 5;
+  $(window).on("scroll", function () {
+    var st = $(this).scrollTop();
+    if (st < lastScrollTop) {
+      $("header, body").addClass("up").removeClass("down");
+    } else if (st > lastScrollTop && st > delta) {
+      $("header, body").addClass("down").removeClass("up");
+    }
+
+    lastScrollTop = st;
+  });
+
+  $(function ($) {
+    //splashHeight();
+    splashContainerPadding();
+
+    if ($(".featured-product-container .display-flex > div").hasClass("col")) {
+      preventDefault();
+    } else {
+      $(
+        "<h3 style='text-align: center'>Sorry, there are currently no promotions or featured products. <br />Please check back soon!</h3>"
+      ).appendTo($(".featured-product-container .display-flex"));
+    }
+  });
+
+  viewport.resize(function () {
+    //splashHeight();
+    splashContainerPadding();
+  });
 })(jQuery);
--- templates/featured-product.php
+++ templates/featured-product.php
@@ -2,7 +2,7 @@
 	<div class="max-width-1440 top-bottom-padding">
 		<div class="display-flex gutter-space-2 align-middle">
 			<?php 
-			if (function_exists(promo_item('featured-product') || promo_item('flyer'))):
+			if ( function_exists(promo_item('featured-product')) || function_exists(promo_item('flyer')) ):
 				promo_item('featured-product');
 				promo_item('flyer');
 			endif; ?>
